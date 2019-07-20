969947c95b002fe500c16df2da88a4c453b132e6
Rex Wang <14366016+rexwangcc@users.noreply.github.com>
2019-07-20 12:28:33 -0400

    Refactor analysis agent, add first set of unit tests and build badges. (#27)
    * Tweak the analysis agent, and bump the version in requirements.
    
    * Refactor analysis agent and add some basic unit tests for it.
    
    * Require Pytest.
    
    * Try to integrate to Travis CI.
    
    * Update .travis.yml
    
    Fix the Travis YAML
    
    * Update .travis.yml
    
    Fix another typo...
    
    * Bump the version of cromwell-tools.
    
    * Fix a test case.
    
    * Bump the version of cromwell-tools.
    
    * Add the missing __init__ file.
    
    * Fix the failing tests.
    
    * Add badges for unit tests and Python version.

--- tracker/common/dcp_agents/analysis_agent.py
+++ tracker/common/dcp_agents/analysis_agent.py
@@ -3,29 +3,37 @@ import logging
 import os
 import json
 
-from cromwell_tools import api as cwm_api
-from cromwell_tools import cromwell_auth as cwm_auth
+import cromwell_tools
+from typing import List, Dict
 
 from tracker.common.config import TrackerInfraConfig
 
 
 class AnalysisAgent:
-    def __init__(self):
+    def __init__(self) -> None:
+        """Interacts with the HCA DCP Pipelines Execution Service."""
         deployment = os.environ['DEPLOYMENT_STAGE']
         self.cromwell_url = 'https://cromwell.caas-prod.broadinstitute.org'
         self.cromwell_collection = 'lira-test' if deployment == 'integration' else f'lira-{deployment}'
-        self.auth = self._get_auth(self.analysis_gcp_creds)
+        self.auth = cromwell_tools.cromwell_auth.CromwellAuth.harmonize_credentials(service_account_key=self.analysis_gcp_creds,
+                                                                                    url=self.cromwell_url)
 
     @property
     def analysis_gcp_creds(self):
         return json.loads(base64.b64decode(TrackerInfraConfig().analysis_gcp_creds).decode())
 
-    def _get_auth(self, service_account_key):
-        """Helper function to generate the auth object to talk to Secondary-analysis service."""
-        return cwm_auth.CromwellAuth.harmonize_credentials(service_account_key=service_account_key,
-                                                           url=self.cromwell_url)
+    def get_workflows_for_project_uuid(self, project_uuid: str, with_labels: bool = True) -> List[Dict[str, str]]:
+        """Query the workflows in secondary analysis by project uuid.
 
-    def get_workflows_for_project_uuid(self, project_uuid, with_labels=True):
+        Args:
+            project_uuid: The UUID of an ingested HCA DCP data project.
+            with_labels: Whether to include all workflow labels information in the result. Note
+                setting this to True will put some extra stress on the secondary analysis service
+                and might be slower to query.
+        
+        Returns:
+            result: A list of workflow metadata blocks that matched the query.
+        """
         query_dict = {
             "label": {
                 "project_uuid": project_uuid
@@ -33,9 +41,10 @@ class AnalysisAgent:
         }
         if with_labels:
             query_dict['additionalQueryResultFields'] = ['labels']
+
+        # to only query within the collection that associated with the current deployment
         query_dict['label']['caas-collection-name'] = self.cromwell_collection
 
-        response = cwm_api.query(query_dict=query_dict, auth=self.auth)
-        response.raise_for_status()
-        result = response.json()
-        return result['results']
+        response = cromwell_tools.api.query(query_dict=query_dict, auth=self.auth, raise_for_status=True)
+        result = response.json()['results']
+        return result
