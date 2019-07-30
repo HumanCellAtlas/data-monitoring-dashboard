e81b28cc4573fcab41bbd93103d37afd2b06f631
parthshahva <parthshahva@gmail.com>
2019-07-30 10:40:52 -0700

    populate workflows_expected field and logic to display red box in UI if true and none/incomplete analysis present

--- tracker/common/dynamo_agents/analysis_dynamo_agent.py
+++ tracker/common/dynamo_agents/analysis_dynamo_agent.py
@@ -1,8 +1,11 @@
 import os
 from collections import defaultdict
 
-from tracker.common.dynamo_agents.dynamo_agent import DynamoAgent
 from tracker.common.dcp_agents.analysis_agent import AnalysisAgent
+from tracker.common.dynamo_agents.dynamo_agent import DynamoAgent
+from tracker.common.dynamo_agents.ingest_dynamo_agent import IngestDynamoAgent
+
+METHODS_SUPPORTED_FOR_WORKFLOWS = ["10X 3' v2 sequencing", "10X v2 sequencing", "Smart-Seq"]
 
 
 class AnalysisDynamoAgent(DynamoAgent):
@@ -13,10 +16,13 @@ class AnalysisDynamoAgent(DynamoAgent):
         self.dynamo_table_name = f"dcp-data-dashboard-analysis-info-{deployment_stage}"
         self.table_display_name = "analysis-info"
         self.analysis_agent = AnalysisAgent()
+        self.ingest_dynamo_agent = IngestDynamoAgent()
 
-    def create_and_save_dynamo_payload(self, project_uuid):
+    def create_and_save_dynamo_payload(self, submission_id, project_uuid):
         print(f"creating analysis info payload for {project_uuid}")
         workflows = self.analysis_agent.get_workflows_for_project_uuid(project_uuid)
+        methods = self.ingest_dynamo_agent.get_item_from_dynamo('submission_id', submission_id)['library_construction_methods']
+        workflows_expected = self._are_workflows_expected_for_project(methods)
         workflow_count_by_status, workflow_count_by_version = self._count_workflows_by_status_and_version(workflows)
         payload = {}
         payload['project_uuid'] = project_uuid
@@ -25,7 +31,15 @@ class AnalysisDynamoAgent(DynamoAgent):
         for version, wf_count in workflow_count_by_version.items():
             payload[version] = wf_count
         payload['total_workflows'] = len(workflows)
+        payload['workflows_expected'] = workflows_expected
         self.write_item_to_dynamo(payload)
+
+    def _are_workflows_expected_for_project(self, project_methods):
+        workflows_expected = False
+        for method in METHODS_SUPPORTED_FOR_WORKFLOWS:
+            if method in project_methods:
+                workflows_expected = True
+        return workflows_expected
 
     def _count_workflows_by_status_and_version(self, workflows):
         workflow_count_by_status = defaultdict(lambda: 0)
