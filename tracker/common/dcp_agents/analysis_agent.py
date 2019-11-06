5410af11eb729b850591e6f42c32d6531d0e5bc0
Rex Wang <rexwangcc@gmail.com>
2019-11-06 16:50:46 -0500

    Fix linting.

--- tracker/common/dcp_agents/analysis_agent.py
+++ tracker/common/dcp_agents/analysis_agent.py
@@ -35,14 +35,14 @@ class AnalysisAgent:
             result: A list of workflow metadata blocks that matched the query.
         """
         # Note: this query dict automatically filters out any workflows that
-        # labeled with the key:values pairs in the `excludeLabelOr` list. 
-        # the labeling mechanism is fully relying on the work of analysis 
-        # component and the data operations team
+        # labeled with the key:values pairs in the `excludeLabelOr` list.
+        # the labeling mechanism is fully relying on the work of analysis
+        # component and the data operations team.
         query_dict = {
             "label": {
                 "project_uuid": project_uuid
             },
-        'excludeLabelOr': ['comment:erased'],
+            "excludeLabelOr": ["comment:erased"],
         }
         if with_labels:
             query_dict['additionalQueryResultFields'] = ['labels']
